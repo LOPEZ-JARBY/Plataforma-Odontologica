b99c8b82a06f3c464f4c2876a6f6b9fc87dc7e9a
Luis20041009894 <hakudoushy19@hotmail.com>
2024-03-16 08:34:21 -0600

    PullToRefreshScreen

---
+++ src/presentation/screens/ui/PullToRefreshScreen.tsx
@@ -0,0 +1,12 @@
+
+import { Text, View } from 'react-native'
+
+export const  PullToRefreshScreen =() => {
+
+    return (
+      <View>
+        <Text>PullToRefreshScreen</Text>
+      </View>
+    )
+  
+}
